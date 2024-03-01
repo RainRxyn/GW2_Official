167613b7ac8537cdd36b0906b3d0be4cb4d02268
Rain <101213684+RainRxyn@users.noreply.github.com>
2024-03-01 15:18:25 +0100

    update user

--- model/models.py
+++ model/models.py
@@ -2,6 +2,7 @@ from datetime import datetime,timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from typing import Optional
 
 # create expense model
 class Expense(db.Model):
@@ -14,9 +15,6 @@ class Expense(db.Model):
     category : so.Mapped[str] = so.mapped_column(sa.String(80), nullable=False,
                                                  index=True)
 
-
-
-
     def __init__(self, name, product, amount, category):
         self.name = name
         self.product = product
@@ -27,3 +25,11 @@ class Expense(db.Model):
         return f'<Expense name={self.name} product={self.product} amount={self.amount} category={self.category}>'
 
 
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
