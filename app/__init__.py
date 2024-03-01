aab6856def771fde0a901ba1f8741da8043a0162
Rain <101213684+RainRxyn@users.noreply.github.com>
2024-03-01 14:21:29 +0100

    update view

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../view")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
