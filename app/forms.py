42a8bb62a8916f25289caa1e666b8d5c3f642534
xandermoons <xander.moons@protonmail.com>
2024-03-06 14:32:22 +0100

    deleted some unused backspaces

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired
 
+
 class ExpenseForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     product = StringField('Product', validators=[DataRequired()])
