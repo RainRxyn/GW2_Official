501d67fb913263682adda4bf62f770a6151705ee
Rain <101213684+RainRxyn@users.noreply.github.com>
2024-03-12 15:09:48 +0100

    Verwijderen van config

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basdir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://expense_tracker:1234@localhost:5432/app.db'
