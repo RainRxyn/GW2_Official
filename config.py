a35418b122c12f95be484fe1351f725b08e5fe74
Ridha <146706393+Ridhakd@users.noreply.github.com>
2024-03-24 23:34:56 +0100

    css

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class Config:
     EXPENSES_PER_PAGE = 5
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:ww01@localhost:5432/app.db'
+                              'sqlite:///' + os.path.join(basedir, 'app.db') or \
+                              'postgresql://expense_tracker:1234@localhost/app.db'
