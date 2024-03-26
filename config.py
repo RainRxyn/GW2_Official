45e8deca9df1aff85ccb5cacd8ff47ba20a89923
xandermoons <xander.moons@protonmail.com>
2024-03-26 09:49:00 +0100

    config edited for presentation

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     EXPENSES_PER_PAGE = 5
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:ww01@localhost:5432/app.db'
+        'postgresql://postgres:postgres@localhost:5432/app.db'
--- tests/testconfig.py
+++ tests/testconfig.py
@@ -1,6 +1,6 @@
 # db values
 
 db_user = 'postgres'
-db_password = 'ww01'
+db_password = 'postgres'
 db_host = 'localhost'
 db_port = '5432'
